3e8dcdb660dab7d5732d95e412cf69ec36f75cc5
Jhon Alex <jhon.1620@live.com>
2019-09-09 23:33:17 -0500

    header 100%

--- src/layouts/header/header.jsx
+++ src/layouts/header/header.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import { Icon } from 'semantic-ui-react'
+import { Icon, Image } from 'semantic-ui-react'
+
+import Logo from '../../image/logo.png'
+import Perfil from '../../image/perfil.png'
 
 class Web extends Component {
   componentDidMount() {
@@ -11,20 +14,41 @@ class Web extends Component {
       <div className="header-main">
         <div className="header-wrap">
           <div className="header-content">
-            <div>
-                ZORRO WEB
+            <div
+              className="header-content-item-1"
+            >
+              <Image
+                src={Logo}
+                size="tiny"
+                style={{
+                  width: '50%',
+                  padding: '9px 10px',
+                }}
+              />
             </div>
-            <div>
+            <div className="header-content-item-2">
               <Icon name="home" />
+              <Icon name="facebook messenger" />
+              <Icon name="bell outline" />
+              <Icon name="setting" />
+              <Icon name="question" />
             </div>
           </div>
-          <div>
-            User
+          <div
+            className="header-content-2"
+          >
+            <Image
+              src={Perfil}
+              style={{
+                width: '17%',
+                borderRadius: '50%',
+              }}
+            />
+            <p>Usuario</p>
           </div>
         </div>
         <div className="header-menu">Menu</div>
       </div>
-      // <h1>HEADER</h1>
     )
   }
 }
