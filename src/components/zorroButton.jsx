d16c9a2a19dab2742dc294a59d3c10fbf72aea68
Jhon Alex <jhon.1620@live.com>
2019-10-10 22:52:54 -0500

    .

--- src/components/zorroButton.jsx
+++ src/components/zorroButton.jsx
@@ -1,29 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Button, Icon } from 'semantic-ui-react'
 
-class ZorroButton extends Component {
-  componentDidMount() {
-    console.log('BODDYYY')
-  }
-
-  render() {
-    const { children, icon } = this.props
-    return (
-      <div className="zorro-button">
-        <Button
-          color="facebook"
-          size="mini"
-        // style={{
-        //   marginLeft: '0px',
-        // }}
-        // loading
-        >
-          <Icon name={icon} />
-          {children}
-        </Button>
-      </div>
-    )
-  }
-}
+const ZorroButton = ({ icon, children }) => (
+  <div className="zorro-button">
+    <Button
+      color="facebook"
+      size="mini"
+    >
+      <Icon name={icon} />
+      {children}
+    </Button>
+  </div>
+)
 
 export default ZorroButton
